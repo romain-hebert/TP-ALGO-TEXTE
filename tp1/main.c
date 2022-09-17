38b11eb4c6b08e1592695565bb6924ab747c25d6
Romain Hebert <romain.hebert0442@gmail.com>
2022-09-17 16:57:39 +0200

    trie avec matrice de transition

--- tp1/main.c
+++ tp1/main.c
@@ -1,9 +1,10 @@
 #include "trie.h"
+#include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-#define MAX_NODES 20
+#define MAX_NODES 50
 
 int main(void) {
     Trie trie = createTrie(MAX_NODES);
@@ -14,10 +15,18 @@ int main(void) {
     unsigned char w[10] = {'\0'};
     strcpy((char *) w, "Test");
     insertInTrie(trie, w);
-    printf("%d\n", isInTrie(trie, w));
+    printf("%s : %d\n", w, isInTrie(trie, w));
     unsigned char w2[10] = {'\0'};
     strcpy((char *) w2, "Testa");
-    printf("%d\n", isInTrie(trie, w2));
+    printf("%s : %d\n\n", w2, isInTrie(trie, w2));
+
+    for (size_t i = 0; i < (size_t) trie->maxNode; i++) {
+        for (size_t j = 0; j < UCHAR_MAX; j++) {
+            printf("%d\t", trie->transition[j][i]);
+        }
+        printf("\n");
+    }
+
     freeTrie(trie);
     return EXIT_SUCCESS;
 }
