7dec636738e3f247a234c473559de0889dd7c591
Romain Hebert <romain.hebert0442@gmail.com>
2022-11-14 11:21:18 +0100

    genere-texte

---
+++ tp3/genere-texte.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define MAX_ALPHA 78
+
+void text_gen(unsigned long len, int alphabet_size);
+
+int main(int argc, char *argv[]) {
+    if (argc != 3) {
+        fprintf(stderr, "Usage : ./genere-texte [--help] TEXT_LENGTH "
+                        "ALPHABET_SIZE\n");
+        exit(EXIT_FAILURE);
+    }
+    unsigned long text_len = strtoul(argv[1], NULL, 10);
+    int alpha_size = atoi(argv[2]);
+
+    text_gen(text_len, alpha_size);
+    return EXIT_SUCCESS;
+}
+
+void text_gen(unsigned long len, int alphabet_size) {
+    if (alphabet_size >= MAX_ALPHA) {
+        // On tombe à court de caractères ASCII lisibles.
+        fprintf(stderr, "Alphabet trop grand!\n");
+        exit(EXIT_FAILURE);
+    }
+    srand((unsigned int) len + (unsigned int) alphabet_size +
+          (unsigned int) time(NULL));
+    size_t i = 0;
+    while (i < len) {
+        printf("%c", (char) (48 + (rand() % alphabet_size)));
+        i++;
+    }
+    printf("\n");
+}
